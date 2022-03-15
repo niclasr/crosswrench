7d7eab0d5c0e756e9cf67e3de87101a85402ba90
Niclas Rosenvik <youremailsarecrap@gmail.com>
2022-03-15 22:48:30 +0100

    Use function for library inclusion instead of file inclusion

--- libs/pstreams.cmake
+++
@@ -1,9 +0,0 @@
-option(EXTERNAL_PSTREAMS "Use pstreams external to the project" OFF)
-
-if (EXTERNAL_PSTREAMS)
-    find_package(pstreams REQUIRED)
-    target_link_libraries(crosswrench PRIVATE pstreams::pstreams)
-else()
-    target_include_directories(crosswrench PRIVATE
-                               ${CMAKE_SOURCE_DIR}/libs/pstreams)
-endif()
